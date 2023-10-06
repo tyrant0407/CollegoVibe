329b677a89c66c4fa96b2d67196052f20022e22a
tyrant0407 <amankushwaha1331@gmail.com>
2023-10-07 02:50:22 +0530

    Error done

--- routes/users.js
+++ routes/users.js
@@ -5,29 +5,36 @@ const passportLocalMongoose = require('passport-local-mongoose');
 mongoose.connect(process.env.DBURL)
   .then(() => console.log('Connected to the database'))
   .catch(err => console.error('Database connection error:', err));
-
+  
 // Define the user schema with validations and indexes
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: [true,'usernamealready taken'], trim: true },
-  name: { type: String, required: true, trim: true },
-  email: { type: String, required: true, unique: [true,'use different email'], trim: true, lowercase: true },
+  username: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String }, // Password will be automatically hashed by passport-local-mongoose
-  bio: { type: String, trim: true },
+  bio: { type: String },
   profileImage: { type: String, default: 'userImage.webp' },
-  posts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }],
-  saved: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }],
-  stories: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Story' }],
-  followings: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-  followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-  socketId: { type: String, trim: true }
-}, { timestamps: true }); // Add timestamps for createdAt and updatedAt
+  posts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Posts' }],
+  saved: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Posts' }],
+  stories: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Storys' }],
+  followings: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Users' }],
+  followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Users' }],
+  socketId: { type: String }
+});
 
 // Apply the passport-local-mongoose plugin for secure password storage and authentication
-userSchema.plugin(passportLocalMongoose, { usernameField: 'email' }); // Use email as the username field
+userSchema.plugin(passportLocalMongoose, {
+  usernameUnique: true,
+  usernameField: 'username', // Specify the username field for authentication
+  passwordValidator: (password, cb) => {
+    // Implement custom password validation if needed
+    // Example: Check password strength, length, etc.
+    cb();
+  }
+});
 
 // Add indexes for frequent queries
 userSchema.index({ username: 1 }); // Index for efficient querying by username
-userSchema.index({ email: 1 }); // Index for efficient querying by email
 
 // Implement error handling for database operations
 userSchema.post('save', (error, doc, next) => {
@@ -39,6 +46,6 @@ userSchema.post('save', (error, doc, next) => {
 });
 
 // Compile the schema into a model
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model('Users', userSchema);
 
 module.exports = User;
